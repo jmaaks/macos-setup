ff52db4defd4fc45aa1b4260d962e5aee2ef1e97
jmaaks <jeff@maaks.com>
2018-04-14 13:07:26 -0700

    Uncomment initial sudo & keep-alive code

--- install.sh
+++ install.sh
@@ -35,8 +35,8 @@ set -e
 
 # Here we go.. ask for the administrator password upfront and run a
 # keep-alive to update existing `sudo` time stamp until script has finished
-# sudo -v
-# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Ensure Apple's command line tools are installed
 if ! command -v cc >/dev/null; then
