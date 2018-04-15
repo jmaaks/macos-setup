de46a97fdb48fb8df2e529c4e070a41002623c5b
Jeff Maaks <jeff@maaks.com>
2018-04-14 18:30:48 -0700

    Update system_settings.sh

--- scripts/system_settings.sh
+++ scripts/system_settings.sh
@@ -53,13 +53,13 @@ echo "This script will make your Mac awesome. Read it carefully first!"
 #fancy_echo "Increasing the window resize speed for Cocoa applications"
 #defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
-fancy_echo "Expanding the save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+#fancy_echo "Expanding the save panel by default"
+#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-fancy_echo "Automatically quit printer app once the print jobs complete"
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+#fancy_echo "Automatically quit printer app once the print jobs complete"
+#defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
 #fancy_echo "Displaying ASCII control characters using caret notation in standard text views"
@@ -95,8 +95,8 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input
 ###############################################################################
 
-fancy_echo "Increasing sound quality for Bluetooth headphones/headsets"
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+#fancy_echo "Increasing sound quality for Bluetooth headphones/headsets"
+#defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 #fancy_echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
 #defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -114,8 +114,8 @@ defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int
 #defaults write -g com.apple.trackpad.scaling 2
 #defaults write -g com.apple.mouse.scaling 2.5
 
-fancy_echo "Turn off keyboard illumination when computer is not used for 5 minutes"
-defaults write com.apple.BezelServices kDimTime -int 300
+#fancy_echo "Turn off keyboard illumination when computer is not used for 5 minutes"
+#defaults write com.apple.BezelServices kDimTime -int 300
 
 ###############################################################################
 # Screen
@@ -125,11 +125,11 @@ fancy_echo "Requiring password immediately after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-fancy_echo "Enabling subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+#fancy_echo "Enabling subpixel font rendering on non-Apple LCDs"
+#defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
-fancy_echo "Enable HiDPI display modes (requires restart)"
-sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+#fancy_echo "Enable HiDPI display modes (requires restart)"
+#sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
 
 
 ###############################################################################
@@ -165,10 +165,10 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 #defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 #defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-fancy_echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+#fancy_echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
+#/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+#/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+#/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 
 #fancy_echo "Disable disk image verification"
@@ -176,14 +176,14 @@ fancy_echo "Enabling snap-to-grid for icons on the desktop and in other icon vie
 #defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 #defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-fancy_echo "Automatically open a new Finder window when a volume is mounted"
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+#fancy_echo "Automatically open a new Finder window when a volume is mounted"
+#defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+#defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+#defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-fancy_echo "Use list view in all Finder windows by default"
+#fancy_echo "Use list view in all Finder windows by default"
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+#defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 
 #fancy_echo "Disable the warning before emptying the Trash"
@@ -202,23 +202,23 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # the Dock to launch apps.
 #defaults write com.apple.dock persistent-apps -array
 
-fancy_echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
-defaults write com.apple.dock tilesize -int 36
+#fancy_echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+#defaults write com.apple.dock tilesize -int 36
 
-fancy_echo "Speeding up Mission Control animations and grouping windows by application"
-defaults write com.apple.dock expose-animation-duration -float 0.1
-defaults write com.apple.dock "expose-group-by-app" -bool true
+#fancy_echo "Speeding up Mission Control animations and grouping windows by application"
+#defaults write com.apple.dock expose-animation-duration -float 0.1
+#defaults write com.apple.dock "expose-group-by-app" -bool true
 
 #fancy_echo "Setting Dock to auto-hide and removing the auto-hiding delay"
 #defaults write com.apple.dock autohide -bool true
 #defaults write com.apple.dock autohide-delay -float 0
 #defaults write com.apple.dock autohide-time-modifier -float 0
 
-fancy_echo "Enable spring loading for all Dock items"
-defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+#fancy_echo "Enable spring loading for all Dock items"
+#defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-fancy_echo "Show indicator lights for open applications in the Dock"
-defaults write com.apple.dock show-process-indicators -bool true
+#fancy_echo "Show indicator lights for open applications in the Dock"
+#defaults write com.apple.dock show-process-indicators -bool true
 
 #fancy_echo "Wipe all (default) app icons from the Dock"
 # This is only really useful when setting up a new Mac, or if you don't use
@@ -228,11 +228,11 @@ defaults write com.apple.dock show-process-indicators -bool true
 #fancy_echo "Don't animate opening applications from the Dock"
 #defaults write com.apple.dock launchanim -bool false
 
-fancy_echo "Make Dock icons of hidden applications translucent"
-defaults write com.apple.dock showhidden -bool true
+#fancy_echo "Make Dock icons of hidden applications translucent"
+#defaults write com.apple.dock showhidden -bool true
 
-fancy_echo "Speed up Mission Control animations"
-defaults write com.apple.dock expose-animation-duration -float 0.1
+#fancy_echo "Speed up Mission Control animations"
+#defaults write com.apple.dock expose-animation-duration -float 0.1
 
 #fancy_echo "Don't group windows by application in Mission Control"
 #fancy_echo "(i.e. use the old Expose behavior instead)"
@@ -244,8 +244,8 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 #fancy_echo "Don't show Dashboard as a Space"
 #defaults write com.apple.dock dashboard-in-overlay -bool true
 
-fancy_echo "Don't automatically rearrange Spaces based on most recent use"
-defaults write com.apple.dock mru-spaces -bool false
+#fancy_echo "Don't automatically rearrange Spaces based on most recent use"
+#defaults write com.apple.dock mru-spaces -bool false
 
 
 
@@ -263,11 +263,11 @@ defaults write com.apple.dock mru-spaces -bool false
 #fancy_echo "Disabling Safari's thumbnail cache for History and Top Sites"
 #defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
-fancy_echo "Enabling Safari's debug menu"
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+#fancy_echo "Enabling Safari's debug menu"
+#defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-fancy_echo "Making Safari's search banners default to Contains instead of Starts With"
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+#fancy_echo "Making Safari's search banners default to Contains instead of Starts With"
+#defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
 #fancy_echo "Removing useless icons from Safari's bookmarks bar"
 #defaults write com.apple.Safari ProxiesInBookmarksBar "()"
@@ -275,13 +275,13 @@ defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 #fancy_echo "Allow hitting the Backspace key to go to the previous page in history"
 #defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
 
-fancy_echo "Enabling the Develop menu and the Web Inspector in Safari"
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+#fancy_echo "Enabling the Develop menu and the Web Inspector in Safari"
+#defaults write com.apple.Safari IncludeDevelopMenu -bool true
+#defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+#defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 
-fancy_echo "Adding a context menu item for showing the Web Inspector in web views"
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+#fancy_echo "Adding a context menu item for showing the Web Inspector in web views"
+#defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 
 ###############################################################################
@@ -296,13 +296,13 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 # Terminal
 ###############################################################################
 
-fancy_echo "Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
-defaults write com.apple.terminal StringEncodings -array 4
+#fancy_echo "Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+#defaults write com.apple.terminal StringEncodings -array 4
 
- echo ""
- echo "Terminal.app: setting the Pro theme by default"
- defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
- defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+# echo ""
+# echo "Terminal.app: setting the Pro theme by default"
+# defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+# defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
 
 ###############################################################################
 # Time Machine
@@ -332,18 +332,18 @@ defaults write com.apple.terminal StringEncodings -array 4
 # Activity Monitor                                                            #
 ###############################################################################
 
-fancy_echo "Show the main window when launching Activity Monitor"
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+#fancy_echo "Show the main window when launching Activity Monitor"
+#defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-fancy_echo "Visualize CPU usage in the Activity Monitor Dock icon"
-defaults write com.apple.ActivityMonitor IconType -int 5
+#fancy_echo "Visualize CPU usage in the Activity Monitor Dock icon"
+#defaults write com.apple.ActivityMonitor IconType -int 5
 
-fancy_echo "Show all processes in Activity Monitor"
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
+#fancy_echo "Show all processes in Activity Monitor"
+#defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-fancy_echo "Sort Activity Monitor results by CPU usage"
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
+#fancy_echo "Sort Activity Monitor results by CPU usage"
+#defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+#defaults write com.apple.ActivityMonitor SortDirection -int 0
 
 
 ###############################################################################
@@ -365,21 +365,21 @@ defaults write com.apple.ActivityMonitor SortDirection -int 0
 #fancy_echo "Disable the sudden motion sensor as it's not useful for SSDs"
 #sudo pmset -a sms 0
 
-fancy_echo "Speeding up wake from sleep to 12 hours from an hour"
+#fancy_echo "Speeding up wake from sleep to 12 hours from an hour"
 # http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
-sudo pmset -a standbydelay 43200
+#sudo pmset -a standbydelay 43200
 
 # echo ""
 # echo "Disable computer sleep and stop the display from shutting off"
 # sudo pmset -a sleep 0
 # sudo pmset -a displaysleep 0
 
-fancy_echo "Disable annoying backswipe in Chrome"
-defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+#fancy_echo "Disable annoying backswipe in Chrome"
+#defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 
-fancy_echo "Use the system-native print preview dialog in Chrome"
-defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+#fancy_echo "Use the system-native print preview dialog in Chrome"
+#defaults write com.google.Chrome DisablePrintPreview -bool true
+#defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
 
 #fancy_echo "Disable the sound effects on boot"
